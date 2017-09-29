5c5efb6b94d5fdf83cb4925476cef5dc16f91954
Michael Saah <mike.saah@gmail.com>
2017-09-29 11:40:59 -0400

    Built up to the point of being able to run error simulations over r, sqrt(r) and r^2

---
+++ LJ/py-stuff/cub/tests/points_vs_error/lj_cub_points_test.py
@@ -0,0 +1,109 @@
+import sys
+sys.path.append('/Users/Mike/Dropbox/TU-17-18/URPFall/LJ/py-stuff/cub/tests/')
+
+import numpy as np
+from scipy import interpolate
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+import matplotlib.pyplot as plt
+import sys
+import math
+from resources import ljhelpers
+
+# for each n, output error graphs and calculate mean error in neighborhood of the minimum of l-j
+
+r_types = ['r','sqrt_r','r_sqr']
+
+for r_type in r_types:
+
+    # setup
+    A,B = 1.0,1.0
+    LJ = ljhelpers.lj(A,B,r_type)
+    lj_min = LJ.min()
+    window_size = 0.2
+    if r_type == 'sqrt_r':
+        window_size = window_size**2
+    if r_type == 'r_sqr':
+        window_size = math.sqrt(window_size)
+    lj_min_neighborhood = (lj_min-(window_size/2), lj_min+(window_size/2))    
+    
+    start,stop = 0.2,3
+    if r_type == 'sqrt_r':
+        start = math.sqrt(start)
+        stop = math.sqrt(stop)
+    if r_type == 'r_sqr':
+        start = start**2
+        stop = stop**2
+    
+    n_test_points = 4000
+    n_vals = [50,100,150,200,300,400,500,750,1000] # values to iterate over
+    #n_vals = [50,100]
+    test_points = np.random.random_sample(n_test_points)*(stop-start) + start
+    bcons = ((1,LJ.ev(start,1)),(1,LJ.ev(stop,1))) # evaluate first derivative of l-j at endpoints 
+    
+    mean_abs_error = []
+    
+    for n in n_vals:
+        # build domain, range, interpolation spline
+        domain = np.linspace(start,stop,n)
+        lj_range = list(map(lambda x: LJ.ev(x,0),domain))
+        cspline = interpolate.CubicSpline(domain,lj_range,bc_type=bcons)
+        #cspline = interpolate.PchipInterpolator(domain,lj_range)
+        test_points_interpolated = []
+        for point in test_points:
+            test_points_interpolated.append(cspline(point))
+        
+        # calculate errors
+        test_points_abs_error = []
+        test_points_rel_error = []
+        neighborhood_abs_error = []   
+    
+        for i,point in enumerate(test_points_interpolated):
+            abs_e = abs(point-LJ.ev(test_points[i],0))
+            rel_e = abs(abs_e / LJ.ev(test_points[i],0))
+            if lj_min_neighborhood[0] < test_points[i] < lj_min_neighborhood[1]:
+                neighborhood_abs_error.append(abs_e)
+            test_points_abs_error.append(abs_e)
+            test_points_rel_error.append(rel_e)
+        mean_abs_error.append(sum(neighborhood_abs_error)/len(neighborhood_abs_error))
+    
+        f, ax = plt.subplots(4, sharex=True)
+        f.set_size_inches(11,8.5)
+        plt.subplots_adjust(hspace=.3)
+        plt.suptitle('Cubic Spline Interpolation of L-J in ' + r_type + ' with n = ' + str(n))
+        ax[0].semilogy(test_points,test_points_abs_error,'ro',markersize=1)
+        ax[0].grid(color='k',linestyle='-', linewidth=1)
+        ax[0].set_title('Absolute Error')
+        ax[1].semilogy(test_points,test_points_rel_error,'bo',markersize=1)
+        ax[1].grid(color='k',linestyle='-', linewidth=1)
+        ax[1].set_title('Relative Error')
+        ax[2].plot(domain,lj_range)
+        ax[2].set_ylim([-1,4])
+        ax[2].grid(color='k',linestyle='-', linewidth=1)
+        ax[2].set_title('Cubic Spline')
+        spline_plot_domain = np.linspace(start,stop,1000)
+        spline_plot_range = list(map(cspline,spline_plot_domain))
+        ax[3].plot(spline_plot_domain,spline_plot_range)
+        ax[2].plot(spline_plot_domain,spline_plot_range)
+        ax[2].plot(lj_min_neighborhood,list(map(lambda x: LJ.ev(x,0),lj_min_neighborhood)),'ro')
+        
+        f.savefig(r_type + "/" + str(n) + ".png")
+        
+    plt.close('all')
+    e_fig = plt.figure()
+    e_fig.set_size_inches(11,8.5)
+    e_plot = e_fig.add_subplot(1,1,1)
+    e_plot.set_title('Mean Absolute Error Around Minimum in '+r_type)
+    e_plot.set_xlabel('Number of Interpolation Points')
+    e_plot.set_ylabel('Mean Absolute Error')
+    e_plot.semilogy(n_vals,mean_abs_error,'b-o')
+    e_plot.table(cellText=[n_vals,["%.1e" % x for x in mean_abs_error]],
+                          rowLabels=['n','Error'],
+                          cellLoc = 'center', rowLoc = 'center',
+                          bbox=[0, -.4, 1, .2],
+                          loc="bottom",
+                 )
+    e_fig.subplots_adjust(bottom=0.3)
+    e_fig.savefig(r_type + '/error.png')
+    
+    
--- LJ/py-stuff/cub/tests/resources/ljhelpers.py
+++ LJ/py-stuff/cub/tests/resources/ljhelpers.py
@@ -1,18 +1,40 @@
+import math
 class lj:
-    def __init__(self,a,b):
+    def __init__(self,a,b,r_type):
+        all_coeffs = {
+            'r' : ((1,-1),(-12,6),(156,-42),(-2184,336)),
+            'r_sqr' : ((1,-1),(-6,3),(42,-12),(-336,60)),
+            'sqrt_r' : ((1,-1),(-24,12),(600,-156),(-15600,2184)),
+        }
+        all_exps = {
+            'r' : (12,6),
+            'r_sqr' : (6,3),
+            'sqrt_r' : (24,12),
+        }
+
         self.a = a
         self.b = b
-    def ev(self,x,order):
-        if order == 0:
-            return self.a/(x**12) - self.b/(x**6)
-        if order == 1:
-            return (-12*self.a)/(x**13) + (6*self.b)/(x**7)
-        if order == 2:
-            return (156*self.a)/(x**14) - (42*self.b)/(x**8)
-        if order == 3:
-            return (-2184*self.a)/(x**15) + (336*self.b)/(x**9)
+        
+        if r_type in ['r','r_sqr','sqrt_r']:
+            self.r_type = r_type
+            self.coeffs = all_coeffs[r_type]
+            self.exps = all_exps[r_type]
         else:
-            raise ValueError("lj needs an order value equal to 0,1,2 or 3")
+            raise ValueError('valid r_types are r, r_sqr, sqrt_r')
+
+
+    def ev(self,x,order):
+        if order in (0,1,2,3):
+            return (self.a*self.coeffs[order][0])/math.pow(x,self.exps[0]+order) + (self.b*self.coeffs[order][1])/math.pow(x,self.exps[1]+order)
+        else:
+            raise ValueError("ev() needs an order value equal to 0, 1, 2 or 3")
+    def min(self):
+        if self.r_type == 'r':
+            return math.pow(2*self.a/self.b,1/6)
+        elif self.r_type == 'r_sqr':
+            return math.pow(2*self.a/self.b,1/3)
+        elif self.r_type == 'sqrt_r':
+            return math.pow(2*self.a/self.b,1/12)
 
 class Poly:
     def __init__(self,coeffs,x0):
